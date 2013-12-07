e403bdd2a11cbaaca5747603d1c3629252216be2
ofri <ofrioster@gmail.com>
2013-12-07 11:12:51 +0200

    Signed-off-by: ofri <ofrioster@gmail.com>

--- run/src/run/Warehouse.java
+++ run/src/run/Warehouse.java
@@ -5,10 +5,38 @@ import java.util.Vector;
 
 public class Warehouse {
 	
-	private Vector<String> kitchenCollectionTolls;
-	private Vector<String> ingredientsAvailable;
+	private Vector<String[]> kitchenCollectionTolls;
+	private Vector<String[]> ingredientsAvailable;
 	
+	public Warehouse(){
+		
+	}
 	
+	public Warehouse(Vector<String[]> kitchenCollectionTolls, Vector<String[]> ingredientsAvailable){
+		
+	}
+
+
+	public void addKitchenTolls(String[] newToll) {
+	}
+	
+	public void addIngredient(String[] ingredient){
+		
+	}
+	
+	public int getNumberOfKitchenTolls(String KitchenTolls){
+		int res=-1;
+		
+		
+		return res;
+	}
+	
+	public int getNumberOfIngredientsAvailable(String ingredients){
+		int res=-1;
+		
+		
+		return res;
+	}
 	
 	
 
